a14f8d3fafa918099b74e799d0b3b50a205dddca
osj2507 <osj@trustpilot.com>
2019-04-06 16:54:35 +0200

    adding twitter to the mix

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,9 @@ module.exports = {
           },
           {
             resolve: "gatsby-remark-responsive-iframe",
+          },
+          {
+            resolve: "@weknow/gatsby-remark-twitter",
           }
         ]
       }
