2a7f80e779586a874758c526be48afd0f949a024
osj2507 <osj@trustpilot.com>
2019-04-13 15:42:09 +0200

    adding meta and share icons

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
 import get from 'lodash/get'
-import Layout from '../components/layout'
+import Helmet from 'react-helmet'
+import ReactShare from 'react-share-simplified';
 
+import Layout from '../components/layout'
 import heroStyles from '../components/hero.module.css'
 import styles from './blog-post.module.css'
 
@@ -15,7 +16,26 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} >
         <div style={{ background: '#fff' }}>
-          <Helmet title={`${post.title} | ${siteTitle}`} />
+          <Helmet>
+            <title>{post.title}</title>
+
+            <meta name="description" content={post.description.childMarkdownRemark.html} />
+
+            <meta property="og:title" content={post.title} />
+            <meta property="og:type" content="article" />
+            <meta property="og:image" content={post.heroImage.file.url} />
+            <meta property="og:url" content={`/blog/${post.slug}`} />
+            <meta property="og:description" content={post.description.childMarkdownRemark.html} />
+            <meta property="og:site_name" content="tennis247.dk" />
+
+            <meta name="twitter:card" content={post.description.childMarkdownRemark.html} />
+            <meta name="twitter:site" content="@tennis247dk" />
+            <meta name="twitter:title" content={post.title} />
+            <meta name="twitter:description" content={post.description.childMarkdownRemark.html} />
+            <meta name="twitter:creator" content="@tennis247dk" />
+            <meta name="twitter:image" content={post.heroImage.file.url} />
+            <meta name="twitter:domain" content="tennis247.dk" />
+          </Helmet>
           <div className={heroStyles.hero}>
             <img className={heroStyles.heroImage} alt={post.title} src={post.heroImage.file.url} />
             <div className={heroStyles.heroImageOverlayLeft} />
@@ -25,6 +45,15 @@ class BlogPostTemplate extends React.Component {
             <h1 className="section-headline">{post.title}</h1>
             <div className={styles.postDate}>
               {post.publishDate}
+            </div>
+            <div className={styles.postShareIcons}>
+              <ReactShare
+                  url={`http://www.tennis247.dk/blog/${post.slug}`}
+                  title={post.title}
+                  facebook={true}
+                  twitter={true}
+                  iconSize={64}
+              />
             </div>
             <div
               className={styles.postDescription}
@@ -51,6 +80,7 @@ export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+      slug
       publishDate(formatString: "MMMM Do, YYYY")
       heroImage {
         id
