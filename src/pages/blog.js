eb37083d55c730f4e1f517b6a1c0d6be73420254
osj2507 <osj@trustpilot.com>
2019-04-23 01:27:52 +0200

    refining the information load

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,24 +8,20 @@ import ArticlePreview from '../components/article-preview'
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const pageInformation = get(this, 'props.data.allContentfulPage.edges')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title'),
+          pageInformation = get(this, 'props.data.allContentfulPage.edges[0].node'),
+          posts = get(this, 'props.data.allContentfulBlogPost.edges');
 
     return (
       <Layout location={this.props.location} >
         <Helmet title={siteTitle} />
-        {pageInformation.map(({ node }) => {
-          return (
-            <Hero data={node} />
-          )
-        })}
+        <Hero data={pageInformation} />
         <div className="wrapper">
           <h2 className="section-headline">Alle artikler</h2>
           <ul className="article-list">
             {posts.map(({ node }) => {
               return (
-                <li key={node.slug}>
+                <li key={node.id}>
                   <ArticlePreview data={node} type="post" />
                 </li>
               )
@@ -59,6 +55,7 @@ export const pageQuery = graphql`
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
+          id
           title
           slug
           publishDate(formatString: "MMMM Do, YYYY")
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,41 +8,31 @@ import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const pageInformation = get(this, 'props.data.allContentfulPage.edges')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const postsYoutube = get(this, 'props.data.allContentfulYoutubePost.edges')
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title'),
+          pageInformation = get(this, 'props.data.allContentfulPage.edges[0].node'),
+          posts = get(this, 'props.data.allContentfulBlogPost.edges'),
+          postsYoutube = get(this, 'props.data.allContentfulYoutubePost.edges'),
+          postsTwitter = get(this, 'props.data.allContentfulTwitterPost.edges');
 
     return (
       <Layout location={this.props.location} >
-        {pageInformation.map(({ node }) => {
-          return (
-            <Helmet>
-              <title>{siteTitle}</title>
-
-              <meta name="description" content="" />
-
-              <meta property="og:title" content={siteTitle} />
-              <meta property="og:image" content={node.heroImage.file.url} />
-              <meta property="og:url" content="/" />
-              <meta property="og:description" content="" />
-              <meta property="og:site_name" content="tennis247.net" />
-
-              <meta name="twitter:card" content="summary" />
-              <meta name="twitter:site" content="@tennis247dk" />
-              <meta name="twitter:title" content={siteTitle} />
-              <meta name="twitter:description" content="" />
-              <meta name="twitter:creator" content="@tennis247dk" />
-              <meta name="twitter:image" content={node.heroImage.file.url} />
-              <meta name="twitter:domain" content="tennis247.net" />
-            </Helmet>
-          )
-        })}
-        {pageInformation.map(({ node }) => {
-          return (
-            <Hero data={node} />
-          )
-        })}
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content="" />
+          <meta property="og:title" content={siteTitle} />
+          <meta property="og:image" content={pageInformation.heroImage.file.url} />
+          <meta property="og:url" content="/" />
+          <meta property="og:description" content="" />
+          <meta property="og:site_name" content="tennis247.net" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@tennis247dk" />
+          <meta name="twitter:title" content={siteTitle} />
+          <meta name="twitter:description" content="" />
+          <meta name="twitter:creator" content="@tennis247dk" />
+          <meta name="twitter:image" content={pageInformation.heroImage.file.url} />
+          <meta name="twitter:domain" content="tennis247.net" />
+        </Helmet>
+        <Hero data={pageInformation} />
         <div className="wrapper">
           <h2 className="section-headline">Artikler</h2>
           <ul className="article-list">
@@ -79,6 +69,7 @@ export const pageQuery = graphql`
     allContentfulPage(filter:{slug:{eq:"home"}}) {
       edges {
         node {
+          id
           title
           heroImage {
             id
