23e421940522eae2c2cb09dc67f66b18e4b8ad2f
osj2507 <osj@trustpilot.com>
2019-05-09 23:05:48 +0200

    changing the hero to hold the title

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,6 +4,7 @@ import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
+    <h1 className={styles.heroTitle}>{data.header}</h1>
     <img className={styles.heroImage} src={data.heroImage.file.url} />
     <div className={styles.heroImageOverlayLeft} />
     <div className={styles.heroImageOverlayRight} />
